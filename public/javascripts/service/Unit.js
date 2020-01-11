86932f71bbbd878c0554544932b1ce74dbabe986
suraj7852 <surajpj7852@gmail.com>
2020-01-11 16:18:52 +0530

    Added functionality for Weight unit conversion

--- public/javascripts/service/Unit.js
+++ public/javascripts/service/Unit.js
@@ -28,12 +28,22 @@ class Unit {
             this.converterArray[1] = volumeObj.find(o => o.type == data.convertTo).values;
             callback(null, this.converterArray);
         }
-        else
-            callback({message:"Invalid Type"});
+
+        if (data.type == Type.Weight) {
+            let weightObj = [
+                {"type": "TONNE", "values": 1000000 },
+                {"type": "KILOGRAM", "values": 1000},
+                {"type": "GRAM", "values": 1}
+            ];
+            this.converterArray[0] = weightObj.find(o => o.type == data.unitType).values;
+            this.converterArray[1] = weightObj.find(o => o.type == data.convertTo).values;
+            callback(null, this.converterArray);
+        } else
+            callback({message: "Invalid Type"});
     }
 }
 
 const Type = {
-    Length: "Length",Volume: "Volume"
+    Length: "Length", Volume: "Volume", Weight:"Weight"
 };
 module.exports = new Unit();
